b4d4d41fbe6288d1c26870e2fddad55dd8c87630
Ryan Dalton <110407320+dalryan@users.noreply.github.com>
2024-12-03 21:58:36 +0100

    Add 2024/03/python

---
+++ 2024/03/python/main.py
@@ -0,0 +1,36 @@
+import re
+
+
+def read_str_from_file(file_path: str) -> str:
+    """Just read a string from a file."""
+    with open(file_path, "r") as f:
+        return f.read()
+
+
+def part_one(s: str) -> int:
+    """It's regex time"""
+    pattern = r"mul\((\d+),(\d+)\)"
+    return sum([int(a) * int(b) for a, b in re.findall(pattern, s)])
+
+
+def part_two(s: str) -> int:
+    """It's still regex time"""
+    values = []
+    pattern = r"(mul|don't|do\b)(?:\((\d+),(\d+)\))?"
+    is_active = True
+    for smt in re.findall(pattern, s):
+        # goofin' around
+        is_active = False if smt[0] == "don't" else (is_active or smt[0] == "do")
+        if not is_active:
+            continue
+        try:
+            values.append(int(smt[1]) * int(smt[2]))
+        except ValueError:
+            continue
+    return sum(values)
+
+
+if __name__ == "__main__":
+    data = read_str_from_file("../test.txt")
+    assert part_one(data) == 161
+    assert part_two(data) == 48
